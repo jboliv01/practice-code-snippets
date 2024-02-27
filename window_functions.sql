25d301a437dbd315386a3180c560ae756927bbbd
jboliv01 <57547273+jboliv01@users.noreply.github.com>
2024-02-27 16:33:54 -0500

    adding windows function used from this tutorialhttps://www.youtube.com/watch?v=dqwhNcZoMOQ&t=3064s

---
+++ window_functions.sql
@@ -0,0 +1,43 @@
+WITH lagged as (
+  SELECT player_name, 
+  season, 
+  pts, 
+  LAG(pts, 1) OVER (
+  PARTITION BY player_name
+  ORDER BY season ) as pts_last_season
+FROM bootcamp.nba_player_seasons 
+),
+
+did_change as (
+  SELECT *, 
+  CASE WHEN pts >= 20 AND   pts_last_season >= 20 THEN 0 ELSE 1 END as pts_stayed_above_20
+  FROM lagged
+),
+
+identified as (
+  SELECT *,
+  SUM(pts_stayed_above_20) OVER (
+  PARTITION BY player_name 
+  ORDER BY season) as streak_identfier 
+  FROM did_change
+),
+
+aggregated as ( 
+SELECT player_name, 
+COUNT(CASE WHEN pts >= 20 THEN 1 END) as consecutive_seasons
+FROM identified 
+GROUP BY player_name, streak_identfier 
+ORDER BY 2 DESC),
+
+ranked as (  
+SELECT *, DENSE_RANK() OVER (
+ORDER BY consecutive_seasons DESC) as dense_rank,
+RANK() OVER (
+ORDER BY consecutive_seasons DESC) as rank,
+ROW_NUMBER() OVER (
+ORDER BY consecutive_seasons DESC) as row_number
+FROM aggregated
+)
+
+SELECT * FROM ranked
+WHERE dense_rank <= 10

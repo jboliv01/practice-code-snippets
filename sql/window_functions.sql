9970a1a744ba7d9036a5789521f8188be665085e
jboliv01 <57547273+jboliv01@users.noreply.github.com>
2024-02-28 10:49:33 -0500

    adding window function snippets

--- sql/window_functions.sql
+++ sql/window_functions.sql
@@ -44,3 +44,92 @@ FROM aggregated
 
 SELECT * FROM ranked
 WHERE dense_rank <= 10
+
+
+-- From the demand2 table, find the cumulative total sum for qty.
+
+WITH rolling_sum as (
+SELECT *, SUM(qty) OVER (
+  ORDER BY day) as cumsum
+  FROM demand2
+  )
+  
+SELECT * FROM lagged;
+
+-- From the demand table, find the cumulative total sum for qty for each product category.
+
+WITH rolling_sum_by_prod as (
+SELECT *, SUM(qty) OVER (
+  PARTITION BY product
+  ORDER BY day) as cumsum
+  FROM demand
+  )
+  
+SELECT * FROM rolling_sum_by_prod;
+
+-- Extract the two worst performing days of each product in terms of number of qty sold. Paraphrasing it: Get the days corresponding to the two minimum most values of qty for each product.
+
+WITH rolling_sum_by_prod as (
+SELECT *, dense_rank() OVER (
+  PARTITION BY product
+  ORDER BY qty ASC) as RN
+  FROM demand
+  )
+  
+SELECT * FROM rolling_sum_by_prod
+WHERE RN <= 2;
+
+-- Sort the table by qty for each product and compute the percentage increase (or decrease) compared to the previous day.
+
+WITH sorted as (
+  SELECT *, LAG(qty, 1) OVER (
+    PARTITION BY product
+    ORDER BY day) as previous_qty
+  FROM demand
+  )
+  
+  SELECT *, SUM(((qty - previous_qty) / previous_qty) * 100) 
+  FROM sorted
+  GROUP BY product, day, qty
+  
+-- Create two new columns in the table that shows the minimum and the maximum quantity sold for each product.
+
+WITH min_max as (
+  SELECT *, MAX(qty) OVER ( 
+    PARTITION BY product) as max_sold,
+	MIN(qty) OVER ( 
+    PARTITION BY product) as min_sold
+  FROM demand
+  )
+  
+  SELECT * from min_max;
+
+-- Calculate the diffence between the second largest and the second smallest sales qty for each product.
+
+WITH diff as (
+  SELECT *, ROW_NUMBER() OVER (
+    PARTITION BY product
+    ORDER BY qty DESC) as row_num_desc,
+    ROW_NUMBER() OVER (
+    PARTITION BY product
+    ORDER BY qty ASC) as row_num_asc
+  FROM demand
+  )
+  
+  SELECT product, day, qty
+  FROM diff
+  WHERE row_num_desc = 2 OR row_num_asc = 2
+
+  -- Create a table to show the day and the names of the product the the highest qty sale.
+
+  
+WITH diff as (
+  SELECT *, DENSE_RANK() OVER (
+    PARTITION BY day
+    ORDER BY qty DESC) as rank_qty
+  FROM demand
+  )
+  
+  SELECT *
+  FROM diff
+  WHERE rank_qty = 1

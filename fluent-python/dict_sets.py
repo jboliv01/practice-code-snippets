5d1f6133e938a1baf9638be93a0d9c98e2b7ab15
Jonah Oliver <jboliv01@gmail.com>
2024-05-10 09:55:59 -0400

    update dict_sets

--- fluent-python/dict_sets.py
+++ fluent-python/dict_sets.py
@@ -16,9 +16,19 @@ class StrKeyDict0(dict):
         return key in self.keys() or str(key) in self.keys()
     
 def main():
+    # custom dict will accept either a string value or int value as a keyß
     d1 = StrKeyDict0([('2', 'two'), ('4', 'four')])
-    d1['2']
+    print('\nCustom Dictionary:\n')
     print(d1)
+    print(f"d1['2'] returns: {d1['2']}")
+    print(f"d1[2] also returns: {d1[2]}")
+    print()
+    # standard dictionary:
+    print('\nStandard dictionary:\n')
+    d2 = dict([('2', 'two'), ('4', 'four')])
+    print(d2)
+    print(f"d2['2'] returns: {d2['2']}")
+    print(f"d2[2] results in a KeyError\n")
 
 if __name__ == "__main__":
     main()
